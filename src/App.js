cc4c0b6fef8a638564ff45f475f520bc65a36455
Sadia-web <sadiamim354@gmail.com>
2020-08-25 17:06:46 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is my React Paragraph</p>
+        <Person name ="Mim" job="Programming"></Person>
+        <Person name ="Sadia" job="Web Developing"></Person>
+       
       </header>
     </div>
   );
-}
-
+  }
+  function Person(Props){
+    return (
+      <div>
+        <h3>My Name: {Props.name}</h3>
+        <p>My Profession: {Props.job} </p>
+      </div>
+    )
+  }
 export default App;
